1a96bda36fa65f32b71f589f7d8c0a0611a4b5ac
du-xiang <2876069311@qq.com>
2023-05-02 17:10:25 +0800

    新建卷积层实现头文件

---
+++ cnn/Conv.h
@@ -0,0 +1,17 @@
+/*
+* Author: ∂≈œÈ
+* 
+* Introduce: This header is used to implement the class of convolutional 
+*		layers in convolutional neural networks
+*/
+
+#include<iostream>
+
+class Conv {
+public:
+
+
+private:
+
+
+}
