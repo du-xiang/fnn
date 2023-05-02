ef81de97f2e9d1d7cf0a5feb55b5f0cf91cf32d1
du-xiang <2876069311@qq.com>
2023-05-02 17:11:12 +0800

    新建卷积神经网络架构实现文件

---
+++ cnn/cnn.cpp
@@ -0,0 +1,18 @@
+/*
+* Author: ∂≈œÈ
+* 
+* Introduce: This file is used to design the network architecture of a 
+*		custom convolutional neural network.
+*/
+
+
+#include "Conv.h"
+
+class cnn {
+public:
+
+
+private:
+
+
+};
