b4f04ed83e2dcc73f35a3430ce2c487914db880b
woodsoil <2876069311@qq.com>
2026-01-19 14:20:31 +0800

    更新：添加训练调试开关

--- main.cpp
+++ main.cpp
@@ -13,13 +13,42 @@
 
 #include "httplib.h"
 
+// 训练调试开关
+#define TRAINING_DEBUG true
+
+// 设置全局变量
+Logger& logger = Logger::getInstance("..//log//log.txt");
+std::shared_ptr<FullConnNN> example(new FullConnNN());
+std::string weightPath = "..//weight//weight.w";
+
+bool trainFNN()
+{
+    Timer t;
+    example->weight_init();
+    logger.log(logLevel::logINFO, __FILE__, __LINE__, "模型权重参数初始化成功");
+
+    example->weight_save("..//weight//weight1.w");
+    logger.log(logLevel::logINFO, __FILE__, __LINE__, "成功保持初始化的模型权重参数");
+
+    example->backward();
+    logger.log(logLevel::logINFO, __FILE__, __LINE__, "反向传播训练完成");
+
+    example->weight_save(weightPath);
+    logger.log(logLevel::logINFO, __FILE__, __LINE__, "模型权重参数存储完成");
+
+    t.pause();
+    std::cout << "time: " << t.elapsedTime() << "ms" << std::endl;
+	logger.log(logLevel::logINFO, __FILE__, __LINE__, "训练完成。本次训练耗时："+std::to_string(t.elapsedTime())+" ms");
+
+    double rate = example->test();
+    logger.log(logLevel::logINFO, __FILE__, __LINE__, "测试完成。正确率: " + std::to_string(rate));
+
+    return true;
+}
+
 int predictFNN(std::vector<double> img)
 {
-    Logger& logger = Logger::getInstance("..//log//log.txt");
-	std::string weightPath = "..//weight//weight.w";
-	std::shared_ptr<FullConnNN> example(new FullConnNN());
-
-	if(example->weight_load(weightPath))
+    if(example->weight_load(weightPath))
 	{
 		logger.log(logLevel::logINFO, __FILE__, __LINE__, "成功加载模型权重参数");
 
@@ -32,27 +61,7 @@ int predictFNN(std::vector<double> img)
 	else
 	{
 		logger.log(logLevel::logWARN, __FILE__, __LINE__, "未成功加载模型权重参数，准备重新训练");
-
-		Timer t;
-		example->weight_init();
-		logger.log(logLevel::logINFO, __FILE__, __LINE__, "模型权重参数初始化成功");
-
-		example->weight_save("..//weight//weight1.w");
-		logger.log(logLevel::logINFO, __FILE__, __LINE__, "成功保持初始化的模型权重参数");
-
-		example->backward();
-		logger.log(logLevel::logINFO, __FILE__, __LINE__, "反向传播训练完成");
-
-		example->weight_save(weightPath);
-		logger.log(logLevel::logINFO, __FILE__, __LINE__, "模型权重参数存储完成");
-
-		t.pause();
-		std::cout << "time: " << t.elapsedTime() << "ms" << std::endl;
-		logger.log(logLevel::logINFO, __FILE__, __LINE__, "训练完成。本次训练耗时："+std::to_string(t.elapsedTime())+" ms");
-
-        double rate = example->test();
-        logger.log(logLevel::logINFO, __FILE__, __LINE__, "测试完成。正确率: " + std::to_string(rate));
-
+        trainFNN();
 		return example->forward(img);
 	}
 
@@ -136,16 +145,22 @@ bool serverInit(httplib::Server &svr)
 
 int main()
 {
-    Logger& logger = Logger::getInstance("..//log//log.txt");
 	logger.log(logLevel::logINFO, __FILE__, __LINE__, "程序开始运行");
 
-	httplib::Server svr;
-    logger.log(logLevel::logINFO, __FILE__, __LINE__, "开启网络连接功能");
-	serverInit(svr);
-	
-    logger.log(logLevel::logINFO, __FILE__, __LINE__, "开启监听,监听127.0.0.1:8080");
-	std::cout << "Server listening on http://127.0.0.1:8080\n";
-    svr.listen("127.0.0.1", 8080);
+    if (TRAINING_DEBUG)
+    {
+        trainFNN();
+    }
+    else
+    {
+        httplib::Server svr;
+        logger.log(logLevel::logINFO, __FILE__, __LINE__, "开启网络连接功能");
+        serverInit(svr);
+        
+        logger.log(logLevel::logINFO, __FILE__, __LINE__, "开启监听,监听127.0.0.1:8080");
+        std::cout << "Server listening on http://127.0.0.1:8080\n";
+        svr.listen("127.0.0.1", 8080);
+    }
 
 	return 0;
 }
